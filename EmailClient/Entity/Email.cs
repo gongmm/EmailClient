d958d56ba6f05548955aff52cffb5914d0f9b63f
2016302580101 <31274146+2016302580101@users.noreply.github.com>
2019-05-24 00:53:28 +0800

    界面更新

---
+++ EmailClient/Entity/Email.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailClient.Entity
+{
+    class Email
+    {
+        public String Sender { get; set; }
+        public String Content{ get; set;}
+        public String ContentBrief { get; set; }
+    }
+}
--- EmailClient/MainWindow.xaml.cs
+++ EmailClient/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using EmailClient.Entity;
 namespace EmailClient
 {
     /// <summary>
@@ -20,9 +20,104 @@ namespace EmailClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<Email> emailList=null;
+        
+              private void createNewEmail(object sender, RoutedEventArgs e)
+        {
+
+            this.SenderText.Text = "";
+            this.ReceiveText.Text = "";
+            this.TopicText.Text = "";
+            this.ContentText.Text = "";
+           this.SendButton.Visibility = Visibility.Visible;
+            this.SendButton.IsEnabled = true;
+            this.CleanButton.Visibility = Visibility.Visible;
+            this.CleanButton.IsEnabled = true;
+        }
+        private void changeSelection(object sender, RoutedEventArgs e)
+        {
+            Email email=(Email)this.receiveBox.SelectedItem;
+            if (email == null)
+                return;
+            this.SenderText.Text = email.Sender;
+            this.ContentText.Text = email.Content;
+            this.SendButton.Visibility = Visibility.Hidden;
+            this.SendButton.IsEnabled = false;
+            this.CleanButton.Visibility = Visibility.Hidden;
+            this.CleanButton.IsEnabled = false;
+        }
         public MainWindow()
         {
             InitializeComponent();
+            this.emails.Height = this.Height - 50 - 50-30;
+            List<Email> emails = new List<Email>();
+            Email email = new Email();
+            email.Sender = "<2016302580101@whu.edu.cn>";
+            email.Content = "This is a test!";
+            int length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0,length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<test@whu.edu.cn>";
+            email.Content = "This is a test!";
+           length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<able@whu.edu.cn>";
+            email.Content = "This is a test!";
+            length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<newone@whu.edu.cn>";
+            email.Content = "This is a test!";
+            length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<another@whu.edu.cn>";
+            email.Content = "This is a test!";
+            length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<thisis@whu.edu.cn>";
+            email.Content = "This is a test!";
+            length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<tryit@whu.edu.cn>";
+            email.Content = "This is a test!";
+            length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            email = new Email();
+            email.Sender = "<ok@whu.edu.cn>";
+            email.Content = "This is a test!";
+            length = 30;
+            if (email.Content.Length < 30)
+                length = email.Content.Length;
+            email.ContentBrief = email.Content.Substring(0, length);
+            emails.Add(email);
+            this.emailList = emails;
+            this.receiveBox.ItemsSource = emails;
         }
+
     }
 }
